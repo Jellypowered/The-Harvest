ff4c46b342d8d828aeb0f8084774575c7e78fce4
Jelly <Jellypowered@gmail.com>
2020-02-29 00:28:23 -0600

    Add project files.

---
+++ TheHarvest/Initialization.cs
@@ -0,0 +1,21 @@
+﻿using HugsLib;
+using HugsLib.Settings;
+using System;
+using Verse;
+using RimWorld;
+
+namespace TheHarvest
+{
+    [StaticConstructorOnStartup]
+    public class Initialization : ModBase
+    {
+        public static SettingHandle<bool> deep_harvest;
+
+        public override string ModIdentifier => "TheHarvest";
+
+        public override void DefsLoaded()
+        {
+            deep_harvest = Settings.GetHandle<bool>("deep_harvest", "TheHarvest.deep_harvest.name".Translate(), "TheHarvest.deep_harvest.desc".Translate(), false);
+        }
+    }
+}
---
+++ TheHarvest/Patch_OrganHarvester.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+
+namespace TheHarvest.HarmonyPatches
+{
+    [HarmonyPatch(typeof(Pawn), "ButcherProducts")]
+    public static class Patch_OrganHarvester
+    {
+        public static void Postfix(ref IEnumerable<Thing> __result, ref Pawn __instance, Pawn butcher, float efficiency)
+        {
+            __result = __result.CompackedItems(__instance);
+        }
+
+        private static IEnumerable<Thing> CompackedItems(this IEnumerable<Thing> list, Pawn pawn)
+        {
+            foreach (Thing thing in list)
+                yield return thing;
+            foreach (Thing thing in pawn.DetachValuableItems())
+                yield return thing;
+        }
+
+        private static IEnumerable<Thing> DetachValuableItems(this Pawn p)
+        {
+            foreach (BodyPartRecord record in p.health.hediffSet.GetNotMissingParts(BodyPartHeight.Undefined, BodyPartDepth.Undefined))
+            {
+                IEnumerable<Hediff> hediffs = from x in p.health.hediffSet.hediffs
+                                              where x.Part == record
+                                              select x;
+                if (hediffs.Any())
+                {
+                    foreach (Hediff hediff in hediffs)
+                    {
+                        if (hediff.def.spawnThingOnRemoved != null)
+                        {
+                            Log.Message("[The Harvest] Obtained: " + hediff.def.spawnThingOnRemoved.defName);
+                            yield return ThingMaker.MakeThing(hediff.def.spawnThingOnRemoved, null);
+                        }
+                    }
+                }
+                else
+                {
+                    if (record.def.spawnThingOnRemoved != null &&
+                        (record.def.alive == false ||
+                        (Initialization.deep_harvest.Value && !p.RaceProps.Animal)))
+                    {
+                        p.health.AddHediff(HediffMaker.MakeHediff(HediffDefOf.MissingBodyPart, p, record), null, null);
+                        Log.Message("[The Harvest] Obtained: " + record.def.spawnThingOnRemoved.defName);
+                        yield return ThingMaker.MakeThing(record.def.spawnThingOnRemoved, null);
+                    }
+                }
+            }
+        }
+    }
+}
